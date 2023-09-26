bb665bcd60da7f64ca3c158216bb4ccc9f62e54d
Christopher Horrell <christopher@horrell.ca>
2023-09-26 16:09:50 -0400

    Update image prep to use ec2launch v2
    Also add better error handling via ErrorActionPreference
    
    Any failed command will fail the script

--- files/prepare-image.ps1
+++ files/prepare-image.ps1
@@ -1,17 +1,9 @@
+$ErrorActionPreference = 'Stop'
+
 $keysFile = [io.path]::combine($env:ProgramData, 'ssh', 'authorized_keys')
 Remove-Item -Recurse -Force -Path $keysFile
 
 Enable-ScheduledTask "Download Key Pair"
 
-echo "Running InitializeInstance"
-& Powershell.exe C:/ProgramData/Amazon/EC2-Windows/Launch/Scripts/InitializeInstance.ps1 -Schedule
-if ($LASTEXITCODE -ne 0) {
-	throw("Failed to run InitializeInstance")
-}
-
 echo "Running Sysprep Instance"
-& Powershell.exe C:/ProgramData/Amazon/EC2-Windows/Launch/Scripts/SysprepInstance.ps1 -NoShutdown
-if ($LASTEXITCODE -ne 0) {
-	throw("Failed to run Sysprep")
-}
-
+& "$Env:Programfiles\Amazon\EC2Launch\ec2launch.exe" sysprep
