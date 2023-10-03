bf57ebcd6389f0004f6fbcec037e967ffa24ab3e
Christopher Horrell <christopher@horrell.ca>
2023-10-03 10:44:46 -0400

    Update Chocolatey install and add notes

--- files/install-choco.ps1
+++ files/install-choco.ps1
@@ -1,9 +1,10 @@
 $ErrorActionPreference = 'Stop'
 
 # Install Chocolatey
+# See https://chocolatey.org/install#individual
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-Invoke-WebRequest -Uri https://community.chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Globally Auto confirm every action
 # See: https://docs.chocolatey.org/en-us/faqs#why-do-i-have-to-confirm-packages-now-is-there-a-way-to-remove-this
