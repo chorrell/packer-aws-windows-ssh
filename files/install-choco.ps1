1d35691a24c3b4f7e64642b1691a2011e4ea9d55
Christopher Horrell <christopher@horrell.ca>
2023-10-03 10:25:24 -0400

    Don't use aliases in PowerShell scripts

--- files/install-choco.ps1
+++ files/install-choco.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 
 # Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-WebRequest https://community.chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
 # Globally Auto confirm every action
 # See: https://docs.chocolatey.org/en-us/faqs#why-do-i-have-to-confirm-packages-now-is-there-a-way-to-remove-this
