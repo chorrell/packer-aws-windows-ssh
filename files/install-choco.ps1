467d378987724507efc87bfbf86ad99ab80f6f1d
Christopher Horrell <christopher@horrell.ca>
2023-10-03 10:39:38 -0400

    Chocolatey install doesn't need to be a one-liner

--- files/install-choco.ps1
+++ files/install-choco.ps1
@@ -1,7 +1,9 @@
 $ErrorActionPreference = 'Stop'
 
 # Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-WebRequest https://community.chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-WebRequest -Uri https://community.chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
 # Globally Auto confirm every action
 # See: https://docs.chocolatey.org/en-us/faqs#why-do-i-have-to-confirm-packages-now-is-there-a-way-to-remove-this
