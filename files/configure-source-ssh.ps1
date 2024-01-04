779832f9b613e564b24874a0e6ea297177fe43f0
Christopher Horrell <christopher@horrell.ca>
2024-01-04 09:59:34 -0500

    Improve comments

--- files/configure-source-ssh.ps1
+++ files/configure-source-ssh.ps1
@@ -31,7 +31,7 @@ $ErrorActionPreference = 'SilentlyContinue'
 Remove-Item -Force $openSSHZip
 $ErrorActionPreference = 'Stop'
 
-# Move into Program Files
+# Move OpenSSH-Win64 into Program Files
 Move-Item -Path (Join-Path $env:TEMP 'OpenSSH-Win64') -Destination $openSSHInstallDir
 
 # Run the install script, terminate if it fails
@@ -40,7 +40,7 @@ if ($LASTEXITCODE -ne 0) {
 	throw("Failed to install OpenSSH Server")
 }
 
-# Add a firewall rule to allow inbound SSH connections to sshd.exe
+# Add a firewall for sshd
 New-NetFirewallRule -Name sshd `
     -DisplayName "OpenSSH Server (sshd)" `
     -Group "Remote Access" `
@@ -52,17 +52,17 @@ New-NetFirewallRule -Name sshd `
     -Program "$openSSHDaemon" `
     -Action Allow
 
-# Ensure sshd automatically starts on boot
+# Start sshd automatically at boot
 Set-Service sshd -StartupType Automatic
 
-# Set the default login shell for SSH connections to Powershell
+# Set the default login shell to Powershell
 New-Item -Path HKLM:\SOFTWARE\OpenSSH -Force
 New-ItemProperty -Path HKLM:\SOFTWARE\OpenSSH `
     -Name DefaultShell `
     -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
 
 $keyDownloadScript = @'
-# Download the instance key pair and authorize Administrator logins using it
+# Download instance key pair to $env:ProgramData\administrators_authorized_keys
 $openSSHAuthorizedKeys = Join-Path $env:ProgramData 'administrators_authorized_keys'
 
 If (-Not (Test-Path $openSSHAdminUser)) {
@@ -78,7 +78,7 @@ $keyMaterial = $streamReader.ReadToEnd()
 
 $keyMaterial | Out-File -Append -FilePath $openSSHAuthorizedKeys -Encoding ASCII
 
-# Ensure access control on authorized_keys meets the requirements
+# Ensure ACL for administrators_authorized_keys is correct
 $acl = New-Object System.Security.AccessControl.DirectorySecurity
 $dacl = New-Object System.Security.AccessControl.FileSystemAccessRule("SYSTEM","FullControl","Allow")
 $acl.SetAccessRule($dacl)
