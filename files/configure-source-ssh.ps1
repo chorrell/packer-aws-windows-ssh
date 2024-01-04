551c24bed55bd789fc61c179ad9819680eabc0c4
Christopher Horrell <christopher@horrell.ca>
2024-01-04 11:17:24 -0500

    enable then start

--- files/configure-source-ssh.ps1
+++ files/configure-source-ssh.ps1
@@ -11,8 +11,8 @@ Set-MpPreference -DisableRealtimeMonitoring $true
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
+Start-Service sshd
 
 # Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
 if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
